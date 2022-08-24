54e9cb80262952c73a597a3605973f1e75e79242
Evgenia Sivtsova <evgenia.sivtsova@gmail.com>
2022-08-24 13:21:24 +0100

    Add edge case tests for Item class

--- lib/item.rb
+++ lib/item.rb
@@ -2,12 +2,42 @@ class Item
   attr_accessor :name, :sell_in, :quality
 
   def initialize(name, sell_in, quality)
+    input_validation(name, sell_in, quality)
+    fail @error if @error != nil
     @name = name
     @sell_in = sell_in
     @quality = quality
   end
 
-  def to_s
+  def to_string
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+  
+  private
+
+  def input_validation(name, sell_in, quality)
+    name_validation(name)
+    sell_in_validation(sell_in)
+    quality_validation(quality)
+  end
+
+  def name_validation(name)
+    @error = "Item's name must be a string" if name.instance_of?(String) == false
+  end
+
+  def sell_in_validation(sell_in)
+    if !sell_in.instance_of?(Integer) 
+      @error = "Sell in days value must be a positive integer"
+    elsif sell_in.negative?
+      @error = "Sell in days value must be a positive integer"
+    end
+  end
+
+  def quality_validation(quality)
+    if !quality.instance_of?(Integer)
+      @error = "Quality value must be a positive integer less than or equal to 50"
+    elsif quality.negative? || quality > 50
+      @error = "Quality value must be a positive integer less than or equal to 50"
+    end
   end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,8 +1,20 @@
 require 'Item'
 
 describe Item do
-  it "constructs" do
-    item = Item.new('orange juice', '10', '30')
-    expect(item.to_s).to eq "orange juice, 10, 30"
+  it "constructs and returns item's attributes as a string" do
+    item = Item.new('orange juice', 10, 30)
+    expect(item.to_string).to eq "orange juice, 10, 30"
+  end
+  
+  it "fails to construct if any of the arguments are missing and raises an Argument error" do
+    expect { Item.new('10', '30') }.to raise_error(ArgumentError)
+  end
+
+  it "raises error when the type of the argument is incorrect or when its value is out of acceptable range" do
+    expect { Item.new(5, 10, 30) }.to raise_error "Item's name must be a string"
+    expect { Item.new('item', 'argument2', 30) }.to raise_error "Sell in days value must be a positive integer"
+    expect { Item.new('item', -5, 30) }.to raise_error "Sell in days value must be a positive integer"
+    expect { Item.new('item', 30, 'argument3') }.to raise_error "Quality value must be a positive integer less than or equal to 50"
+    expect { Item.new('item', 30, -1) }.to raise_error "Quality value must be a positive integer less than or equal to 50"
   end
 end
